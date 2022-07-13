9ee2726ffc0f0dbe669af525d72200ce9eb20aed
ldd003 <873652231@qq.com>
2022-07-13 08:02:20 +0800

    step2,step3,安装router，store等

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-1. 安装 vite, 选择 vue
+1. 安装 vite, 选择 vue，名字为 vite-project
    `npm create vite`
+2. 用 vuecli5 创建一个 vue3 项目，选 features 时选中 Router Vuex CSS Processors(less)
+   `vue create demovue3`
+   - 把 vite-project 的 src 替换为 demovue3 的 src
+3. 安装 core-js（模块化标准库） vue-router（路由） pinia(vuex,新状态管理) less(样式预处理器)
+   `npm i core-js vue-router pinia`
+   `npm i less -D`
+   - 在 vite.config.js 中，配置路径别名 alias，@
+     `resolve: { alias: { "@": resolve(__dirname, "src"), }, }`
+   * router 中 createRouter 的 history,没有 process, 改为
+     `history: createWebHistory("/")`
+   * 引入 pinia，写 pinia 定义和使用 demo
---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>name: {{ name }} - {{ nameLowerCase }} - {{ nameUpperCase }}</p>
+    <p>age: {{ age }}</p>
+    <button @click="changeName">改变名字</button>
+    <button @click="changeAge">改变年龄</button>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import { useUser } from "../store/storeA";
+const store = useUser();
+
+let { name, age, nameLowerCase, nameUpperCase } = storeToRefs(store);
+
+const changeName = () => {
+  name.value += "1";
+};
+
+const changeAge = () => {
+  store.updateNum(10);
+};
+</script>
